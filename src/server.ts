25ae40e31bcbd4affddb02d929ead4ce107d51ad
Kaio Guilherme Bauer <kaiobbauer@gmail.com>
2024-07-01 13:57:16 -0300

    Initial Commit

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const MONGODB_URI =
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/flexilease';
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
