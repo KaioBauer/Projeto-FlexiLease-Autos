f5943219e9191fdcfeee9a8e03d37999c3aa14c6
Kaio Guilherme Bauer <kaiobbauer@gmail.com>
2024-07-02 13:18:46 -0300

    feat: including everything from the car

---
+++ src/models/car.ts
@@ -0,0 +1,41 @@
+// models/Car.ts
+import mongoose, { Schema, Document, Model } from 'mongoose';
+
+interface IAccessory {
+  _id: mongoose.Types.ObjectId;
+  description: string;
+}
+
+interface ICar extends Document {
+  modelName: string;
+  color: string;
+  year: number;
+  value_per_day: number;
+  accessories: IAccessory[];
+  number_of_passengers: number;
+}
+
+const accessorySchema = new Schema<IAccessory>(
+  {
+    description: { type: String, required: true },
+  },
+  { _id: true },
+);
+
+const carSchema = new Schema<ICar>(
+  {
+    modelName: { type: String, required: true },
+    color: { type: String, required: true },
+    year: { type: Number, required: true, min: 1950, max: 2023 },
+    value_per_day: { type: Number, required: true },
+    accessories: { type: [accessorySchema], required: true },
+    number_of_passengers: { type: Number, required: true },
+  },
+  {
+    toJSON: { virtuals: true, versionKey: false },
+    toObject: { virtuals: true },
+  },
+);
+
+export const Car: Model<ICar> = mongoose.model<ICar>('Car', carSchema);
+export default Car;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import carRoutes from './routes/carRoutes';
+import { generateToken } from './utils/generateToken';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+let token = generateToken('123');
+
+app.use(express.json());
+app.use('/api/v1', carRoutes);
+
 const MONGODB_URI =
   process.env.MONGODB_URI || 'mongodb://localhost:27017/flexilease';
 
@@ -13,6 +20,7 @@ mongoose
   .connect(MONGODB_URI)
   .then(() => {
     console.log('Connected to MongoDB');
+    console.log(token);
   })
   .catch((error) => {
     console.error('Error connecting to MongoDB:', error);
@@ -27,3 +35,5 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+export default app;
